c2e1d28939af5d26846777290fac8dafc91d6281
david@randombits.org <david@randombits.org@d16e70e7-8953-0410-8c39-1f250fa3d1bd>
2008-08-13 18:46:01 +0000

    * Fixed bug with not detecting @Facade on interfaces inherited from a superclass.

--- trunk/src/main/java/org/randombits/facade/Facadable.java
+++ trunk/src/main/java/org/randombits/facade/Facadable.java
@@ -25,7 +25,6 @@ package org.randombits.facade;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -39,6 +38,5 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.PARAMETER}) 
-@Inherited 
 @Documented public @interface Facadable {
 }
